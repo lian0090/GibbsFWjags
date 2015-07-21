65707d0cbaefe217157befe21ecfc647b80ebc01
lian0090 <lian0090@umn.edu>
2015-07-21 16:10:33 -0400

    updated wheatfunctions

--- R/FunctionsForwheat.R
+++ R/FunctionsForwheat.R
@@ -1,83 +1,82 @@
-subsetVAR=function(VAR,ENV,y,n.subsetVAR,seed,G){
+subsetVAR=function(VAR,ENV,y,n.subsetVAR,seed,G=NULL,pro.missing,savedir,runModels=T)
+{
 	out=list()
- if(!missing(seed)){
-	set.seed(seed)
+	if(!missing(seed)){
+		set.seed(seed)
 	}
 	VAR=as.character(VAR)
 	ENV=as.character(ENV)
-   
-   nobs=length(VAR)
-   VARlevels=unique(VAR)
-   VARselect=unique(VAR)[sample(1:length(VARlevels),n.subsetVAR)]
-   whselect=which( VAR %in% VARselect)
-   out$VAR=VAR[whselect]
-   out$ENV=ENV[whselect]
-   out$y=y[whselect]
-  
- 
-     if(!missing(G)){
-	out$G=G[VARselect,VARselect]
- 
- }
- return(out) 
-   
-}
-
-sample.missing=function(VAR,ENV,y,pro.missing,seed,G,savedir,runModels=T){
-	##VAR should be a vector or character specifying the name of VAR in data
-	##ENV should be a vector or character specifying the name of ENV in data
-	##y should be a vector or character specifying the name of y in the data
-if(!missing(seed)){
-	set.seed(seed)
-}
-VAR=as.character(VAR)
-ENV=as.character(ENV)
-dat=data.frame(cbind(VAR=VAR,ENV=ENV,y=y),stringsAsFactors=F)
-dat$y=as.numeric(y)
-nobs=length(y)
-VARlevels=unique(VAR)
-ng=length(VARlevels)
-ENVlevels=unique(ENV)
-nh=length(ENVlevels)
-
-#randomly sample pro.missing Environments as missing for each VAR
-nh.remove=round(nh*pro.missing)
-if(nh.remove<1)nh.remove=1
- datfull=dat
- whichmissing=vector()
- for(i in 1:ng){
- whichmissing=c(whichmissing,which((dat$ENV %in% ENVlevels[sample(1:nh,nh.remove)]) & (dat$VAR==VARlevels[i])))
- }
- train=rep(1,400)
- train[whichmissing]=0
- datfull$train=train
- #for(i in 1:ng){
- #	dat=dat[-which((dat$ENV %in% ENVlevels[sample(1:nh,nh.remove)]) & (dat$VAR==VARlevels[i])),]			
- #	}
- 
-dat=datfull[-whichmissing,]
-save(G,file=file.path(savedir,"G.rda"))
-save(datfull,file=file.path(savedir,"datfull.rda"))
-save(dat,file=file.path(savedir,"dat.rda"))
-
-if(runModels==T){
-lm1.1=FW(dat$y,dat$VAR,dat$ENV,method="OLS")
-lm2.1=FW(dat$y,dat$VAR,dat$ENV,saveAt=file.path(savedir,"GibbsI"),method="Gibbs")
-
-	if(!missing(G)){
-		lm2.2=FW(dat$y,dat$VAR,dat$ENV,A=G,saveAt=file.path(savedir,"GibbsA"),method="Gibbs")
-		corr3=summaryCor(datfull$y,datfull$VAR,datfull$ENV,predictedValue=lm2.2)
-	}else{
-	corr3=rep(NA,3)
+  	VARlevels=unique(VAR)
+	VARselect=unique(VAR)[sample(1:length(VARlevels),n.subsetVAR)]
+	whselect=which( VAR %in% VARselect)
+	VAR=VAR[whselect]
+	ENV=ENV[whselect]
+	y=y[whselect]
+	dat=data.frame(cbind(VAR=VAR,ENV=ENV,y=y),stringsAsFactors=F)
+	dat$y=as.numeric(y)
+	dat$yNA=dat$y
+	if(!is.null(G)){
+		G=G[VARselect,VARselect]
+		save(G,file=file.path(savedir,"G.rda"))
 	}
-	
-corr1=summaryCor(datfull$y,datfull$VAR,datfull$ENV,predictedValue=lm1.1)
-corr2=summaryCor(datfull$y,datfull$VAR,datfull$ENV,predictedValue=lm2.1)
-corr=rbind(corr1,corr2,corr3)
-rownames(corr)=c("lm","Gibbs","GibbsV")
-return(corr)
-
+	nobs=length(y)
+	VARlevels=unique(VAR)
+	ng=length(VARlevels)
+	ENVlevels=unique(ENV)
+	nh=length(ENVlevels)
+	#randomly sample pro.missing Environments as missing for each VAR
+	nh.remove=round(nh*pro.missing)
+	if(nh.remove<1)nh.remove=1
+	whichmissing=vector()
+	if(!missing(seed)){
+		set.seed(seed)
+	}
+	for(i in 1:ng){
+ 		whichmissing=c(whichmissing,which((dat$ENV %in% ENVlevels[sample(1:nh,nh.remove)]) & (dat$VAR==VARlevels[i])))
+ 	}
+	dat$yNA[whichmissing]=NA
+	save(dat,file=file.path(savedir,"dat.rda"))
+   
+	if(runModels==T){
+		OLS=FW(dat$yNA,VAR,ENV,method="OLS")
+		save(OLS,file=file.path(savedir,"OLS.rda"))
+		GibbsI=FW(dat$yNA,VAR,ENV,saveAt=file.path(savedir,"GibbsI"),method="Gibbs")
+        save(GibbsI,file=file.path(savedir,"GibbsI.rda"))
+		if(!missing(G)){
+			GibbsA=FW(dat$yNA,VAR,ENV,A=G,saveAt=file.path(savedir,"GibbsA"),method="Gibbs")
+			save(GibbsA,file=file.path(savedir,"GibbsA.rda"))
+			corr3=get_corYhatYmean(GibbsA,yFull=y)
+		}else{
+			corr3=rep(NA,3)
+		}
+		corr1=get_corYhatYmean(OLS,yFull=y)
+		corr2=get_corYhatYmean(GibbsI,yFull=y)
+		corr=rbind(corr1,corr2,corr3)
+		rownames(corr)=c("OLS","GibbsI","GibbsA")
+		return(corr)
+	}
+   
 }
 
-	
+get_corYhatYmean=function(FWobj,yFull)
+{
+	for(i in 1:length(FWobj)){
+		assign(names(FWobj)[i],FWobj[[i]])
+	}
+	y=yFull
+	if(length(whichNa)>0){
+		ymean_train=aggregate(y[-whichNa],by=list(VAR[-whichNa],ENV[-whichNa]),mean)[,3]
+		ymean_vali=aggregate(y[whichNa],by=list(VAR[whichNa],ENV[whichNa]),mean)[,3]
+		yhat_train=aggregate(yhat[-whichNa,],by=list(VAR[-whichNa],ENV[-whichNa]),mean)[,-c(1:2)]
+		yhat_vali=aggregate(yhat[whichNa,],by=list(VAR[whichNa],ENV[whichNa]),mean)[,-c(1:2)]
+		corr=c(cor(ymean_train,yhat_train),cor(yhat_vali,ymean_vali))
+ 	}else{
+ 		yhat_mean=aggregate(yhat,by=list(VAR,ENV),mean)[,3]
+		ymean=aggregate(y,by=list(VAR,ENV),mean)[,3]
+		corr=cor(yhat_mean,ymean)
+		corr=c(corr,corr)
+	}
+	names(corr)=c(rep("train",ncol(yhat)),rep("vali",ncol(yhat)))
+	return(corr)
 }
+
--- R/plotcorr.R
+++ R/plotcorr.R
@@ -1,32 +1,34 @@
 
 require(R.utils)
-plotcorr=function(corr,methoddim=2,cornamesdim=3,methods,corname,plotfile){
+plotcorr=function(corr,methoddim=2,cornamesdim=3,methods,corname,plotfile)
+{
 	if(!missing(plotfile)){
 		pdf(plotfile,width=9,height=3)
 	}
 	tmp=extract(corr,dims=c(methoddim,cornamesdim),indices=list(methods[1],corname),drop=T)
 	nobs=length(tmp)
-	 plot(tmp,ylim=range(corr),xlim=c(1,nobs*1.1),col=1)
-if(length(methods>1)){
-for(i in 2:length(methods)){
-tmp=extract(corr,dims=c(methoddim,cornamesdim),indices=list(methods[i],corname),drop=T)
-points(tmp,col=i)
- }
- }
- legend(x="bottomright",col=c(1:length(methods)),pch=rep(1,length(methods)),legend=methods)
- if(!missing(plotfile)){
- 	dev.off()
-system(paste("open",filename))
- }
+	plot(tmp,ylim=range(corr),xlim=c(1,nobs*1.1),col=1)
+	if(length(methods>1)){
+		for(i in 2:length(methods)){
+			tmp=extract(corr,dims=c(methoddim,cornamesdim),indices=list(methods[i],corname),drop=T)
+			points(tmp,col=i)
+ 		}
+ 	}
+ 	legend(x="bottomright",col=c(1:length(methods)),pch=rep(1,length(methods)),legend=methods)
+ 	if(!missing(plotfile)){
+ 		dev.off()
+		system(paste("open",filename))
+	}
 }
 
-plotdiffcorr=function(corr,methoddim,cornamesdim,methods,corname,...){
-		tmp1=extract(corr,dims=c(methoddim,cornamesdim),indices=list(methods[1],corname),drop=T)
-        tmp2=extract(corr,dims=c(methoddim,cornamesdim),indices=list(methods[2],corname),drop=T)
-        diffcorr=tmp2-tmp1
-        n.sample=length(diffcorr)
-	    boxplot(diffcorr,...)
-        abline(h=0,col="red")
-        points(diffcorr~seq(0.5,1.5,length.out=n.sample),pch=3,col='red')
+plotdiffcorr=function(corr,methoddim,cornamesdim,methods,corname,...)
+{
+	tmp1=extract(corr,dims=c(methoddim,cornamesdim),indices=list(methods[1],corname),drop=T)
+    tmp2=extract(corr,dims=c(methoddim,cornamesdim),indices=list(methods[2],corname),drop=T)
+    diffcorr=tmp2-tmp1
+    n.sample=length(diffcorr)
+	boxplot(diffcorr,...)
+    abline(h=0,col="red")
+    points(diffcorr~seq(0.5,1.5,length.out=n.sample),pch=3,col='red')
 }
 
